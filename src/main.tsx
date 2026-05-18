ed89ec75a860b55a5aa2d130f128281c419f9ab4
vironsided <vironsided@github.com>
2026-05-18 18:46:57 +0400

    Revert "fix(ui): apply scroll reset before initial render"
    This reverts commit 0754fe4eb88d03c3fbe354c0af3d275110268ef6.

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,6 @@ import 'leaflet/dist/leaflet.css'
 import './index.css'
 import App from './App.tsx'
 
-if ("scrollRestoration" in window.history) {
-  window.history.scrollRestoration = "manual";
-}
-window.scrollTo(0, 0);
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
